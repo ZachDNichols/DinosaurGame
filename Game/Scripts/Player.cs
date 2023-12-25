f540c634070ba4827f4a16b177214baa357b094d
Zach Nichols <zachdnichols@gmail.com>
2023-12-25 01:59:52 -0600

    Redid project in C#

---
+++ Game/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		MovePlayer();
+	}
+
+	private void MovePlayer()
+	{
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 velocity = Velocity;
+		
+		if (direction != Vector2.Zero)
+		{
+			velocity = direction.Normalized() * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
